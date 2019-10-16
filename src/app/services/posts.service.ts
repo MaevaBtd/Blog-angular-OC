8a8d45903acb8dae7fe2fdefd4cb2f4289602380
Maeva BERTRAND <mbertrand@forfaitrennes>
2019-10-16 17:24:45 +0200

    add firebase install

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Post } from '../models/Post.model';
+import { Subject } from 'rxjs/';
+import * as firebase from 'firebase';
+import DataSnapshot = firebase.database.DataSnapshot;
 
 @Injectable()
 export class PostsService {
@@ -7,4 +11,42 @@ export class PostsService {
   postsSubject = new Subject<Post[]>();
 
   constructor() { }
+
+  createNewPost(newPost: Post) {
+    this.posts.push(newPost);
+    this.savePosts();
+    this.emitPosts();
+  }
+
+  emitPosts() {
+    this.postsSubject.next(this.posts);
+  }
+
+  savePosts() {
+    firebase.database().ref('/posts').set(this.posts);
+  }
+
+  getPosts() {
+    firebase.database().ref('/posts')
+      .on('value', (data: DataSnapshot) => {
+          this.posts = data.val() ? data.val() : [];
+          this.emitPosts();
+        }
+      );
+  }
+
+  removePost(post: Post) {
+    const postIndexToRemove = this.posts.findIndex(
+      (postEl) => {
+        if (postEl === post) {
+          return true;
+        }
+      }
+    );
+    this.posts.splice(postIndexToRemove, 1);
+    this.savePosts();
+    this.emitPosts();
+  }
+
+
 }
